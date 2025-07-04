2512a1f6f2ea36b5a10358da78e747436a3db3b3
Tegar Wahyu <tegarwahyu107@gmail.com>
2025-07-04 13:08:22 +0700

    feat: add validation for customer data in Create and Update

--- pkg/models/customer.go
+++ pkg/models/customer.go
@@ -3,8 +3,8 @@ package models
 type Customer struct {
 	No          int    `json:"no" gorm:"primaryKey;column:no;autoIncrement"`
 	Name        string `json:"name" binding:"required" gorm:"column:name;not null"`
-	NIK         string `json:"nik" binding:"required" gorm:"column:nik;not null;unique"`
-	PhoneNumber string `json:"phone_number" binding:"required" gorm:"column:phone_number;not null"`
+	NIK         string `json:"nik" binding:"required" gorm:"column:nik;not null;unique;size:16"`
+	PhoneNumber string `json:"phone_number" binding:"required" gorm:"column:phone_number;not null;size:15"`
 }
 
 func (Customer) TableName() string {
