972a5ccaaf6920fc37583809dd48eea6f2fee8ce
Tegar Wahyu <tegarwahyu107@gmail.com>
2025-07-04 17:11:03 +0700

    feat: add Membership fields to Customer model

--- pkg/models/customer.go
+++ pkg/models/customer.go
@@ -5,6 +5,9 @@ type Customer struct {
 	Name        string `json:"name" binding:"required" gorm:"column:name;not null"`
 	NIK         string `json:"nik" binding:"required,len=16" gorm:"column:nik;not null;unique;size:16"`
 	PhoneNumber string `json:"phone_number" binding:"required,max=15" gorm:"column:phone_number;not null;size:15"`
+	
+	MembershipID *int        `json:"membership_id" gorm:"column:membership_id"`
+	Membership   *Membership `json:"membership,omitempty" gorm:"foreignKey:MembershipID;references:No"`
 }
 
 func (Customer) TableName() string {
